2fc001863abd463cd43e20c68028844c1f868e8b
Bharath Reddy <bharathreddi18@gmail.com>
2026-08-03 16:25:16 +0530

    fix(backend): sort imports and use collections.abc.AsyncGenerator

--- server/database.py
+++ server/database.py
@@ -1,5 +1,6 @@
 import os
-from typing import AsyncGenerator
+from collections.abc import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
